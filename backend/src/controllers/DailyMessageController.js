80da9020456a49d9acc78041a2e4c3e87bbb1cfd
Leandro Rovagnoli <leandro.rovagnoli@gmail.com>
2020-02-02 04:27:00 -0300

    Fixing json return object for 'DailyMessage'; Fixing update route;

--- backend/src/controllers/DailyMessageController.js
+++ backend/src/controllers/DailyMessageController.js
@@ -2,8 +2,8 @@ const DailyMessage = require('../models/DailyMessage')
 
 module.exports = {
     async index(req, res) {
-        const dailyMessage = await DailyMessage.find();
-        return res.json(dailyMessage);
+        const dailyMessages = await DailyMessage.find();
+        return res.json(dailyMessages);
     },
 
     async getByDate(req, res) {
@@ -19,7 +19,7 @@ module.exports = {
                 { $or: [{ dateMessage }, { dailyMessage }] },
             ]
         },
-            function (err, obj) {
+            function (err) {
                 if (err)
                     return res.json({ message: 'Houve uma falha ao atualizar. Entre em contato com o desenvolvedor!', err: err })
             });
@@ -31,55 +31,45 @@ module.exports = {
                 dateMessage
             }
 
-            await DailyMessage.create(newDailyMessage,
-                function (err, obj) {
-                    if (err)
-                        return res.json({ message: 'Houve uma falha ao atualizar. Entre em contato com o desenvolvedor!', err: err })
-                });
+            const obj = await DailyMessage.create(newDailyMessage)
+                .catch(function (err) {
+                    return res.json({ message: 'Houve uma falha ao atualizar. Entre em contato com o desenvolvedor!', err: err })
+                })
 
-            return res.json({ message: 'Pensamento matinal criado com sucesso!' })
+            return res.json(obj);
         }
 
-        return res.json({ message: 'Pensamento matinal já existente!' })
+        return res.json({ message: 'Mensagem ou dia já existente!' })
     },
 
-    async updateByDate(req, res) {
+    async update(req, res) {
         const { author, dailyMessage, dateMessage } = req.body;
 
-        const dailyMessageDb = await DailyMessage.findOne({ dateMessage: req.params.dateMessage },
-            function (err, obj) {
+        const newDailyMessage = {
+            $set: {
+                author,
+                dailyMessage,
+                dateMessage
+            }
+        }
+
+        const dailyMessageUpdated = await DailyMessage.findByIdAndUpdate({ _id: req.params.id }, newDailyMessage,
+            function (err) {
                 if (err)
                     return res.json({ message: 'Houve uma falha ao atualizar. Entre em contato com o desenvolvedor!', err: err })
             });
 
-        if (dailyMessageDb) {
-            const newDailyMessage = {
-                $set: {
-                    author,
-                    dailyMessage,
-                    dateMessage
-                }
-            }
+        return res.json(dailyMessageUpdated);
 
-            await DailyMessage.updateOne(dailyMessageDb, newDailyMessage,
-                function (err, obj) {
-                    if (err)
-                        return res.json({ message: 'Houve uma falha ao atualizar. Entre em contato com o desenvolvedor!', err: err })
-                });
-
-            return res.json({ message: 'Pensamento matinal atualizado com sucesso!' })
-        }
-
-        return res.json({ message: 'Pensamento matinal não encontrado!' })
     },
 
     async deleteByDate(req, res) {
         await DailyMessage.findOneAndDelete({ dateMessage: req.params.dateMessage })
             .then(messageDeleted => {
                 if (messageDeleted)
-                    return res.json({ message: 'Pensamento removido com sucesso!' })
+                    return res.json({ message: 'Mensagem removida com sucesso!' })
                 else
-                    return res.json({ message: 'Pensamento matinal não encontrado!' })
+                    return res.json({ message: 'Mensagem não encontrada!' })
             })
             .catch(err => {
                 return res.json({ message: 'Houve uma falha ao remover. Entre em contato com o desenvolvedor!', err: err })
@@ -87,23 +77,15 @@ module.exports = {
     },
 
     async deleteById(req, res) {
-        if (req.params.id) //delete by id
-        {
-            await DailyMessage.findByIdAndDelete(req.params.id)
-                .then(messageDeleted => {
-                    if (messageDeleted)
-                        return res.json({ message: 'Pensamento removido com sucesso!' })
-                    else
-                        return res.json({ message: 'Pensamento matinal não encontrado!' })
-                })
-                .catch(err => {
-                    return res.json({ message: 'Houve uma falha ao remover. Entre em contato com o desenvolvedor!', err: err })
-                })
-        }
-        else // delete by date
-        {
-
-        }
-
+        await DailyMessage.findByIdAndDelete(req.params.id)
+            .then(messageDeleted => {
+                if (messageDeleted)
+                    return res.json({ message: 'Mensagem removida com sucesso!' })
+                else
+                    return res.json({ message: 'Mensagem não encontrada!' })
+            })
+            .catch(err => {
+                return res.json({ message: 'Houve uma falha ao remover. Entre em contato com o desenvolvedor!', err: err })
+            })
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,7 +6,7 @@ const routes = Router();
 routes.get('/', DailyMessageController.index);
 routes.get('/mensagem/data/:dateMessage', DailyMessageController.getByDate);
 routes.post('/', DailyMessageController.store);
-routes.put('/mensagem/data/:dateMessage', DailyMessageController.updateByDate);
+routes.put('/mensagem/:id', DailyMessageController.update);
 routes.delete('/mensagem/data/:dateMessage', DailyMessageController.deleteByDate);
 routes.delete('/mensagem/:id', DailyMessageController.deleteById);
 
